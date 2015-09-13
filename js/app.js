6cbe6aa50e2f910e222d97ce945f70b8b0f805d1
Karthik Krishna <karthikkrishna611@gmail.com>
2015-09-13 17:04:23 -0400

    Tick Your RIde : Initial App

---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tickYourRideApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tickYourRideApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ js/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tickYourRideApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tickYourRideApp
+ */
+angular.module('tickYourRideApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Dubai',
+      'Europe',
+      'Singapore',
+      'Malasia',
+      'Sri Lanka'
+    ];
+  });
